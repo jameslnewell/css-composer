7cf51705f8d3d79931fbf38c3e2d47d3762b0832
James Newell <james@digitaledgeit.com.au>
2016-03-29 23:36:36 +1100

    finish @import plugin

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,6 @@
 'use strict';
 const fs = require('fs');
+const chalk = require('chalk');
 const composer = require('..');
 
 const file = process.argv[2];
@@ -7,7 +8,19 @@ const file = process.argv[2];
 composer()
   .process(fs.readFileSync(file), {from: file})
   .then(
-    result => console.log(result),
-    error => console.log(error)
-  )
+    result => {
+
+      console.log('RESULT:', result.css, result.messages);
+
+      if (result.messages.length > 0) {
+        result.messages.forEach(message => console.error(chalk.red(message.toString())));
+        process.exit(-1);
+      }
+
+    },
+    error => {
+      console.error(chalk.red(error));
+      process.exit(-1);
+    }
+)
 ;
--- index.js
+++ index.js
@@ -1,56 +1,31 @@
 'use strict';
-const resolve = require('resolve');
 const postcss = require('postcss');
+const cssImport = require('./lib/postcss-import-plugin');
+const cssInlineComment = require('postcss-inline-comment');
 const cssVariables = require('postcss-simple-vars');
 const cssConditionals = require('postcss-conditionals');
-const atImport = require('postcss-import');
 const cssMixins = require('postcss-sassy-mixins');
 const cssNested = require('postcss-nested');
 const cssCalc = require('postcss-calc-function').default;
-const inlineComment = require('postcss-inline-comment');
 
-function resolveModule(module, basedir) {
-  return new Promise((res, rej) => {
+module.exports = function() {
 
-    const options = {
-      basedir,
-      extensions: ['.scss', '.sass', '.css'],
+  //TODO: functions (@function, @return), interpolation, inline calculations, @error, @warn
+
+  return postcss([
+    cssImport({resolve: {
       packageFilter: pkg => {
         pkg.main = pkg['main.scss'] || pkg['main.sass'] || pkg['main.css'] || pkg['style'];
         return pkg;
       }
-    };
-
-    resolve(module, options, (err, path) => {
-      if (err) {
-        rej(err);
-      } else {
-        res(path);
-      }
-
-    });
-
-  });
-
-  return result;
-}
-
-module.exports = function() {
-
-  //TODO: interpolation, inline calculations
-
-  return postcss([
-    atImport({resolve: resolveModule})
+    }}),
+    //cssInlineComment(), //FIXME: currently only works inside rules
+    cssMixins(),
+    cssVariables(),
+    cssConditionals(),
+    cssNested(),
+    cssCalc()
   ]);
-
-
-    //.use(inlineComment()) //TODO: currently only works inside rules
-    //.use(cssMixins())
-    //.use(cssVariables())
-    //.use(cssConditionals())
-    //.use(cssNested())
-    //.use(cssCalc())
-
 
   //consider: node-css-mqpacker
 };
